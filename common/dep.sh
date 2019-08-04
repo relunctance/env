019962505d7f2fc225c2f148660b0598bf015a87
Gao.QiLin <334136724@qq.com>
2019-08-04 14:17:33 +0800

    Update dep.sh

--- common/dep.sh
+++ common/dep.sh
@@ -2,13 +2,13 @@
 base_dir=$(cd "$(dirname "$0")";pwd)
 yumbin=`which yum`
 if [ "$yumbin" != "" ];then
-	sudo yum install bash_completion*
+	sudo yum install bash_completion* jq
 fi
 
 brewbin=`which brew`
 
 if [ "$brewbin" != "" ];then
-	brew install bash-completion
+	brew install bash-completion jq
 fi
 
 # 注意有些文件不能直接拷贝, 比如 .bash_profile
