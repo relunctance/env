6e5a483978827861d7da5a7377520984f0170fd1
Gao.QiLin <334136724@qq.com>
2021-01-24 20:17:46 +0800

    Update dep.sh

--- common/dep.sh
+++ common/dep.sh
@@ -2,13 +2,13 @@
 base_dir=$(cd "$(dirname "$0")";pwd)
 yumbin=`which yum`
 if [ "$yumbin" != "" ];then
-	sudo yum install bash_completion* jq lrzsz
+	sudo yum install bash_completion* bash-completion-extras jq lrzsz
 fi
 
 brewbin=`which brew`
 
 if [ "$brewbin" != "" ];then
-	brew install bash-completion jq lrzsz
+	brew install bash-completion bash-completion-extras  jq lrzsz
 fi
 
 # 注意有些文件不能直接拷贝, 比如 .bash_profile
